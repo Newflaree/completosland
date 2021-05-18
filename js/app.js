c062a065bd3ad292ced93903d105b0f908fca909
Newflare <camiloj.lopez79@gmail.com>
2021-05-18 18:52:41 -0400

    Se crea la sección del menú

--- js/app.js
+++ js/app.js
@@ -29,3 +29,32 @@ window.addEventListener( 'scroll', stickyHeader );
 /*--------------------------------------
             Toggler Navbar 
 --------------------------------------*/
+
+
+/*-----------------------------------------------------------------------------------------*/
+
+
+/*--------------------------------------
+              Menu Tabs 
+--------------------------------------*/
+const menuTabs = document.querySelector( '.menu-tabs' );
+
+const activeDataTarget = ( e ) => {
+  if ( e.target.classList.contains( 'menu-tab-item' ) && 
+       !e.target.classList.contains( 'active' ) ) {
+
+    const target = e.target.getAttribute( 'data-target' );
+
+    menuTabs.querySelector( '.active' ).classList.remove( 'active' );
+    e.target.classList.add( 'active' );
+
+    const menuSection = document.querySelector( '.menu-section' );
+    menuSection.querySelector( '.menu-tab-content.active' ).classList.remove( 'active' );
+    menuSection.querySelector( target ).classList.add( 'active' );
+  }
+}
+
+menuTabs.addEventListener( 'click', activeDataTarget );
+/*--------------------------------------
+              Menu Tabs 
+--------------------------------------*/
